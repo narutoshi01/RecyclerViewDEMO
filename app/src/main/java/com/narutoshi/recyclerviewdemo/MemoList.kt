e7355550af99f3e14b7410e74dcf5fe6af724bdb
sakamichi-jewel <taigayyy@gmail.com>
2020-05-14 10:42:25 +0900

    set adaptor and LayoutManager for RecyclerView

--- app/src/main/java/com/narutoshi/recyclerviewdemo/MemoList.kt
+++ app/src/main/java/com/narutoshi/recyclerviewdemo/MemoList.kt
@@ -1,28 +1,28 @@
 package com.narutoshi.recyclerviewdemo
 
 class MemoList {
-    val memoList = ArrayList<MemoModel>()
+    val memoItems = ArrayList<MemoModel>()
 
     fun setMemoList() {
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo1", content = "Memo1 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo2", content = "Memo2 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo3", content = "Memo3 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo4", content = "Memo4 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo5", content = "Memo5 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo6", content = "Memo6 Content")
         )
-        memoList.add(
+        memoItems.add(
             MemoModel(title = "Memo7", content = "Memo7 Content")
         )
     }
